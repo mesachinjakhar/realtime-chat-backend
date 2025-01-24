7a26ed009aa58f02bb76026fb280ea389e32d1b6
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-01-24 16:20:11 +0530

    socket io setuped

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require("express");
-
-const PORT = 3000;
+const { Server } = require("socket.io");
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 
-const startServer = async () => {
-  app.listen(PORT, () => {
-    console.log("Server is running on port:", PORT);
-  });
-};
+const PORT = 3001;
+let client = null;
 
-startServer();
+// Start http server
+const server = app.listen(PORT, () => {
+  console.log("Server is running on port: ", PORT);
+});
+
+// Pass http server to socket.io
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("new connection");
+  client = socket;
+});
+
+module.exports = client;
