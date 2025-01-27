16d421c62d9f3ce399d91d0a7a5b0190fa1aa4b1
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-01-27 13:13:13 +0530

    console logging removed

--- src/socket/events/addUserEvents.js
+++ src/socket/events/addUserEvents.js
@@ -2,7 +2,6 @@ users = require("../../users");
 
 module.exports = (io, socket) => {
   socket.on("add-user", (username) => {
-    console.log("users array before adding: ", users);
     users.push({ id: socket.id, username: username });
     io.emit("active-users", users);
   });
--- src/socket/events/messageEvents.js
+++ src/socket/events/messageEvents.js
@@ -1,7 +1,6 @@
 module.exports = (io, socket) => {
   // Handle 'message' event
   socket.on("new-message", (msg) => {
-    console.log(`Message received: ${msg.to} ${msg.message}`);
     socket.to(msg.to).emit("message", {
       type: "server",
       to: msg.to,
